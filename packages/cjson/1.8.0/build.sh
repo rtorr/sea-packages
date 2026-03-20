fcbcb3fd22cefa74112aef350536e4af923d2d84
rtorr <me@rtorr.com>
2026-03-19 19:25:17 -0700

    Use powershell for downloads on Windows (curl in Git Bash has TLS issues)

--- packages/cjson/1.8.0/build.sh
+++ packages/cjson/1.8.0/build.sh
@@ -3,20 +3,26 @@ set -e
 VERSION="1.7.17"
 SRCDIR="$SEA_PROJECT_DIR/_src"
 
+download() {
+    URL="$1"; DEST="$2"
+    if command -v powershell.exe >/dev/null 2>&1; then
+        powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '$URL' -OutFile '$DEST'"
+    else
+        curl -fSL --retry 3 -o "$DEST" "$URL"
+    fi
+}
+
 if [ ! -d "$SRCDIR/cJSON-${VERSION}" ]; then
     mkdir -p "$SRCDIR"
-    curl -sSfL -o "$SRCDIR/cjson.tar.gz" \
-        "https://github.com/DaveGamble/cJSON/archive/refs/tags/v${VERSION}.tar.gz"
+    download "https://github.com/DaveGamble/cJSON/archive/refs/tags/v${VERSION}.tar.gz" "$SRCDIR/cjson.tar.gz"
     tar xzf "$SRCDIR/cjson.tar.gz" -C "$SRCDIR"
 fi
 
 cd "$SRCDIR/cJSON-${VERSION}"
-mkdir -p _build && cd _build
+rm -rf _build && mkdir _build && cd _build
 
 CMAKE_ARGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_INSTALL_PREFIX=$SEA_INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_CJSON_TEST=OFF"
-if [ -n "$CC" ]; then
-    CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_C_COMPILER=$CC"
-fi
+[ -n "$CC" ] && CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_C_COMPILER=$CC"
 
 cmake .. $CMAKE_ARGS 2>&1
 cmake --build . --config Release -j4 2>&1
--- packages/lz4/1.10.0/build.sh
+++ packages/lz4/1.10.0/build.sh
@@ -3,20 +3,26 @@ set -e
 VERSION="1.10.0"
 SRCDIR="$SEA_PROJECT_DIR/_src"
 
+download() {
+    URL="$1"; DEST="$2"
+    if command -v powershell.exe >/dev/null 2>&1; then
+        powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '$URL' -OutFile '$DEST'"
+    else
+        curl -fSL --retry 3 -o "$DEST" "$URL"
+    fi
+}
+
 if [ ! -d "$SRCDIR/lz4-${VERSION}" ]; then
     mkdir -p "$SRCDIR"
-    curl -sSfL -o "$SRCDIR/lz4.tar.gz" \
-        "https://github.com/lz4/lz4/archive/refs/tags/v${VERSION}.tar.gz"
+    download "https://github.com/lz4/lz4/archive/refs/tags/v${VERSION}.tar.gz" "$SRCDIR/lz4.tar.gz"
     tar xzf "$SRCDIR/lz4.tar.gz" -C "$SRCDIR"
 fi
 
 cd "$SRCDIR/lz4-${VERSION}"
-mkdir -p _build && cd _build
+rm -rf _build && mkdir _build && cd _build
 
 CMAKE_ARGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_INSTALL_PREFIX=$SEA_INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DLZ4_BUILD_CLI=OFF -DLZ4_BUILD_LEGACY_LZ4C=OFF"
-if [ -n "$CC" ]; then
-    CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_C_COMPILER=$CC"
-fi
+[ -n "$CC" ] && CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_C_COMPILER=$CC"
 
 cmake ../build/cmake $CMAKE_ARGS 2>&1
 cmake --build . --config Release -j4 2>&1
